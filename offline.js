824b8f6f6722c296d590f0472cb848bb29e2de9c
pisonchristian <37868557+pisonchristian@users.noreply.github.com>
2018-08-04 09:24:12 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1521882026,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bckgrnd-sheet0.png",
+		"images/d7-sheet0.png",
+		"images/d8-sheet0.png",
+		"images/d4-sheet0.png",
+		"images/clear-sheet0.png",
+		"images/hud-sheet0.png",
+		"media/explosion 5.m4a",
+		"media/explosion 5.ogg",
+		"media/hit1.m4a",
+		"media/hit1.ogg",
+		"media/ok - 1.m4a",
+		"media/ok - 1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
